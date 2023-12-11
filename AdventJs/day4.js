287449f31999f73567da68eacb12c8d3061169a1
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2023-12-11 01:24:28 -0500

    advent js, ejercicio del dia 4, completado

--- AdventJs/day4.js
+++ AdventJs/day4.js
@@ -1,49 +1,26 @@
 function decode (message) {
   // Code here
-  const myMessage = message.split(' ')
-  const result = myMessage.map(item => {
-    return algo(item)
-  }).join(' ')
-  return result
+  let myMessage = message
+  while (myMessage.includes('(')) {
+    const indexFin = myMessage.indexOf(')')
+    const indexIni = myMessage.slice(0, indexFin).lastIndexOf('(')
+    const beforeMessage = myMessage.slice(indexIni, indexFin + 1)
+    let afterMessage = myMessage.slice(indexIni + 1, indexFin)
+    // revert message
+    afterMessage = afterMessage.split('').reverse().join('')
+    myMessage = myMessage.replace(beforeMessage, afterMessage)
+  }
+  return myMessage
 }
 
-const algo = (mess) => {
-  let arrAlgo = mess.split('')
-  let i = 0
-  if (mess.includes(')(') || mess.includes('()')) {
-    const indexMid = arrAlgo.slice(0, mess.indexOf(')(') + 1)
-    const indexPrev = indexMid.lastIndexOf('(')
-    const newMessage1 = indexMid.slice(indexPrev).join('')
-    console.log(mess.split(')('))
-    const newMessage2 = arrAlgo.slice(mess.indexOf(')(') + 1, -1).join('')
-    // console.log(typeof algo(newMessage1).concat(algo(newMessage2)).split(''))
-    arrAlgo = [...arrAlgo.slice(0, indexPrev),
-      [algo(newMessage1).concat(algo(newMessage2))],
-      ...arrAlgo.slice(mess.indexOf(')('), -1)]
-    // console.log(mess.indexOf(')('))
-  }
-  while (arrAlgo.includes('(') || arrAlgo.includes(')')) {
-    if (i === 6) break
-    const indexalgo = i % 2 === 0 ? arrAlgo.indexOf('(') : arrAlgo.indexOf(')')
-    const lasintex = i % 2 === 0 ? arrAlgo.lastIndexOf(')') : arrAlgo.lastIndexOf('(')
-    const newArr = arrAlgo.slice(indexalgo + 1, lasintex).reverse()
-    // console.log(newArr)
-    arrAlgo = [...arrAlgo.slice(0, indexalgo), ...newArr, ...arrAlgo.slice(lasintex + 1)]
-    i++
-    // if (arrAlgo.slice(indexalgo + 1).includes('(')) {
-    //   console.log('entro')
-    // }
-  }
-  return arrAlgo.join('')
-}
-// const a = decode('hola (odnum)')
-// console.log(a) // hola mundo
+const a = decode('hola (odnum)')
+console.log(a) // hola mundo
 
-// const b = decode('(olleh) (dlrow)!')
-// console.log(b) // hello world!
+const b = decode('(olleh) (dlrow)!')
+console.log(b) // hello world!
 
-// const c = decode('sa(u(cla)atn)s')
-// console.log(c) // santaclaus
+const c = decode('sa(u(cla)atn)s')
+console.log(c) // santaclaus
 
 // Paso a paso:
 // ? 1. Invertimos el anidado -> sa(ualcatn)s
